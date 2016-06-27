ae531f00d6d9437a1640d91c3b97c10fc551eb76
Daniel Matuschek <daniel@matuschek.net>
2016-06-27 13:48:47 +0200

    rdware definition for the Witty cloud module

---
+++ hue/wittycloud.h
@@ -0,0 +1,22 @@
+// GPIO definitions for the Witty Cloud module
+
+// RGB
+#define redPIN    15
+#define greenPIN  12
+#define bluePIN   13
+
+// W FET
+#define w1PIN     16
+#define w2PIN     14
+
+// onbaord green LED D1
+#define LEDPIN    2
+// onbaord red LED D2
+#define LED2PIN   2
+
+
+#define SERIAL Serial
+// note
+// TX GPIO2 @Serial1 (Serial ONE)
+// RX GPIO3 @Serial
+
