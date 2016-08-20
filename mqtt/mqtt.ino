970201e31aaf6067d650dfd87e0498fe2ffeda64
Daniel Matuschek <daniel@hifiberry.com>
2016-08-20 09:41:42 +0200

    Implemented OTA updates

--- mqtt/mqtt.ino
+++ mqtt/mqtt.ino
@@ -6,14 +6,25 @@
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>   // Local WebServer used to serve the configuration portal
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
 #include <WiFiManager.h>        // WiFi Configuration Magic
 #include <PubSubClient.h>       // MQTT client
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+#define DEVELOPMENT
 
 WiFiManager wifiManager;
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
+ESP8266WebServer httpServer(80);
+ESP8266HTTPUpdateServer httpUpdater;
 
 const char* mqtt_server = "192.168.1.89";
+// Password for update server
+const char* username = "admin";
+const char* password = "";
 
 // MQTT topics
 // state, brightness, rgb
@@ -35,8 +46,8 @@ char* MQTT_LIGHT_W2_COMMAND_TOPIC = "XXXXXXXX/w2/light/switch";
 char* MQTT_LIGHT_W2_BRIGHTNESS_STATE_TOPIC = "XXXXXXXX/w2/brightness/status";
 char* MQTT_LIGHT_W2_BRIGHTNESS_COMMAND_TOPIC = "XXXXXXXX/w2/brightness/set";
 
-
 char* chip_id = "00000000";
+char* myhostname = "esp00000000";
 
 // buffer used to send/receive data with MQTT
 const uint8_t MSG_BUFFER_SIZE = 20;
@@ -55,8 +66,8 @@ const char* LIGHT_OFF = "OFF";
 #define W1_PIN               14
 #define W2_PIN               4
 
-#define LEDPIN    2
-#define LED2PIN   2
+#define GREEN_PIN    1
+#define RED_PIN      5
 
 // store the state of the rgb light (colors, brightness, ...)
 boolean m_rgb_state = false;
@@ -73,10 +84,6 @@ uint8_t m_w2_brightness = 255;
 
 void setup()
 {
-
-  pinMode(LEDPIN, OUTPUT);
-  pinMode(LED2PIN, OUTPUT);
-
   pinMode(RGB_LIGHT_RED_PIN, OUTPUT);
   pinMode(RGB_LIGHT_GREEN_PIN, OUTPUT);
   pinMode(RGB_LIGHT_BLUE_PIN, OUTPUT);
@@ -86,18 +93,33 @@ void setup()
   pinMode(W2_PIN, OUTPUT);
   setW2(0);
 
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+  digitalWrite(RED_PIN, 0);
+  digitalWrite(GREEN_PIN, 1);
+
+  sprintf(chip_id, "%08X", ESP.getChipId());
+  sprintf(myhostname, "esp%08X", ESP.getChipId());
+
   // Setup console
   Serial1.begin(115200);
   delay(10);
   Serial1.println();
   Serial1.println();
 
+  // reset if necessary
+  // wifiManager.resetSettings();
+
   wifiManager.setTimeout(3600);
   WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
   wifiManager.addParameter(&custom_mqtt_server);
+  WiFiManagerParameter custom_password("password", "password for updates", password, 40);
+  wifiManager.addParameter(&custom_password);
+  wifiManager.setCustomHeadElement(chip_id);
   wifiManager.autoConnect();
 
   mqtt_server = custom_mqtt_server.getValue();
+  password = custom_password.getValue();
 
   Serial1.println("");
 
@@ -110,7 +132,6 @@ void setup()
   // init the MQTT connection
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-  sprintf(chip_id, "%08X", ESP.getChipId());
 
   // replace chip ID in channel names
   memcpy(MQTT_LIGHT_RGB_STATE_TOPIC, chip_id, 8);
@@ -130,6 +151,16 @@ void setup()
   memcpy(MQTT_LIGHT_W2_BRIGHTNESS_STATE_TOPIC, chip_id, 8);
   memcpy(MQTT_LIGHT_W2_BRIGHTNESS_COMMAND_TOPIC, chip_id, 8);
 
+  digitalWrite(RED_PIN, 1);
+
+  // OTA
+  // do not start OTA server if no password has been set
+  if (password != "") {
+    MDNS.begin(myhostname);
+    httpUpdater.setup(&httpServer,username,password);
+    httpServer.begin();
+    MDNS.addService("http", "tcp", 80);
+  }
 }
 
 // function called to adapt the brightness and the colors of the led
@@ -153,48 +184,48 @@ void setW2(uint8_t brightness) {
 // function called to publish the state of the led (on/off)
 void publishRGBState() {
   if (m_rgb_state) {
-    client.publish(MQTT_LIGHT_RGB_STATE_TOPIC, LIGHT_ON);
+    client.publish(MQTT_LIGHT_RGB_STATE_TOPIC, LIGHT_ON, true);
   } else {
-    client.publish(MQTT_LIGHT_RGB_STATE_TOPIC, LIGHT_OFF);
+    client.publish(MQTT_LIGHT_RGB_STATE_TOPIC, LIGHT_OFF, true);
   }
 }
 
 void publishW1State() {
   if (m_w1_state) {
-    client.publish(MQTT_LIGHT_W1_STATE_TOPIC, LIGHT_ON);
+    client.publish(MQTT_LIGHT_W1_STATE_TOPIC, LIGHT_ON, true);
   } else {
-    client.publish(MQTT_LIGHT_W1_STATE_TOPIC, LIGHT_OFF);
+    client.publish(MQTT_LIGHT_W1_STATE_TOPIC, LIGHT_OFF, true);
   }
 }
 
 void publishW2State() {
   if (m_w2_state) {
-    client.publish(MQTT_LIGHT_W2_STATE_TOPIC, LIGHT_ON);
+    client.publish(MQTT_LIGHT_W2_STATE_TOPIC, LIGHT_ON, true);
   } else {
-    client.publish(MQTT_LIGHT_W2_STATE_TOPIC, LIGHT_OFF);
+    client.publish(MQTT_LIGHT_W2_STATE_TOPIC, LIGHT_OFF, true);
   }
 }
 
 // function called to publish the colors of the led (xx(x),xx(x),xx(x))
 void publishRGBColor() {
   snprintf(m_msg_buffer, MSG_BUFFER_SIZE, "%d,%d,%d", m_rgb_red, m_rgb_green, m_rgb_blue);
-  client.publish(MQTT_LIGHT_RGB_RGB_STATE_TOPIC, m_msg_buffer);
+  client.publish(MQTT_LIGHT_RGB_RGB_STATE_TOPIC, m_msg_buffer, true);
 }
 
 // function called to publish the brightness of the led (0-100)
 void publishRGBBrightness() {
   snprintf(m_msg_buffer, MSG_BUFFER_SIZE, "%d", m_rgb_brightness);
-  client.publish(MQTT_LIGHT_RGB_BRIGHTNESS_STATE_TOPIC, m_msg_buffer);
+  client.publish(MQTT_LIGHT_RGB_BRIGHTNESS_STATE_TOPIC, m_msg_buffer, true);
 }
 
 void publishW1Brightness() {
   snprintf(m_msg_buffer, MSG_BUFFER_SIZE, "%d", m_w1_brightness);
-  client.publish(MQTT_LIGHT_W1_BRIGHTNESS_STATE_TOPIC, m_msg_buffer);
+  client.publish(MQTT_LIGHT_W1_BRIGHTNESS_STATE_TOPIC, m_msg_buffer, true);
 }
 
 void publishW2Brightness() {
   snprintf(m_msg_buffer, MSG_BUFFER_SIZE, "%d", m_w2_brightness);
-  client.publish(MQTT_LIGHT_W2_BRIGHTNESS_STATE_TOPIC, m_msg_buffer);
+  client.publish(MQTT_LIGHT_W2_BRIGHTNESS_STATE_TOPIC, m_msg_buffer, true);
 }
 
 // function called when a MQTT message arrived
@@ -204,7 +235,7 @@ void callback(char* p_topic, byte* p_payload, unsigned int p_length) {
   for (uint8_t i = 0; i < p_length; i++) {
     payload.concat((char)p_payload[i]);
   }
-  
+
   // handle message topic
   if (String(MQTT_LIGHT_RGB_COMMAND_TOPIC).equals(p_topic)) {
     // test if the payload is equal to "ON" or "OFF"
@@ -298,6 +329,10 @@ void callback(char* p_topic, byte* p_payload, unsigned int p_length) {
     setColor(m_rgb_red, m_rgb_green, m_rgb_blue);
     publishRGBColor();
   }
+
+  digitalWrite(GREEN_PIN, 0);
+  delay(1);
+  digitalWrite(GREEN_PIN, 1);
 }
 
 void reconnect() {
@@ -343,11 +378,31 @@ void reconnect() {
   }
 }
 
+uint16_t i = 0;
 
 void loop()
 {
+  // process OTA updates
+  httpServer.handleClient();
+  
+  i++;
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
+
+  // Post the full status to MQTT every 65535 cycles. This is roughly once a minute
+  // this isn't exact, but it doesn't have to be. Usually, clients will store the value
+  // internally. This is only used if a client starts up again and did not receive
+  // previous messages
+  delay(1);
+  if (i == 0) {
+    publishRGBState();
+    publishRGBBrightness();
+    publishRGBColor();
+    publishW1State();
+    publishW1Brightness();
+    publishW2State();
+    publishW2Brightness();
+  }
 }
